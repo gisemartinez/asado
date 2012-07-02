bf877e3751d7b6b59dc3fc9f564f2148ffba1d48
Gisela Martinez <gise@gise-K53SC.(none)>
2012-07-02 14:11:31 -0300

    actualizacion

--- controles/menu.js
+++ controles/menu.js
@@ -3,26 +3,32 @@
  * and open the template in the editor.
  */
 
-steal('calcAsado/vistas/imagenes/styles.css',
+steal('asado/vistas/imagenes/styles.css',
       'can/control/control.js',
       'can/view/ejs',
-      'fredhq-roundabout/jquery.roundabout.js',
-      'fredhq-roundabout-shapes/jquery.roundabout-shapes.js',
-      'calcAsado/commons/commons.js'
+      'asado/commons/commons.js'
 ).then(function(){
-        can.Control('Menu_slide',
+        can.Control('Menu',
         {
          defaults:{}
         },
         {
          'init':function(element,options){
-            this.element.append(can.view(url+'vistas/menu.ejs', {img :url}))
+            this.element.append(can.view('vistas/menu.ejs', {img :url}))
             $.mobile.changePage($('#menuPage'),{transition:"flip"})
-            $("ul").roundabout({shape:'lazySusan'});
 
         },
-         'li click':function(){
-                console.log('li')
+	'li#config click':function(){
+                alert("Configurar app")
+         },
+	'li#historial click':function(){
+                alert("Mostrar historial")
+         },
+	'li#info click':function(){
+                alert("Info del sistema")
+         },
+         'img click':function(){
+                alert("Hacer Asado")
          }
         })
  })
